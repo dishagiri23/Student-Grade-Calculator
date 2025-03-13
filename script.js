6ab2628deee3e1d3269f29325f94d5b6f9243e35
dishagiri23 <dishagiri09170@gmail.com>
2025-03-14 00:18:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function cal(){
+    let phy = document.getElementById("phy").value;
+     chem = document.getElementById("chem").value;
+     math = document.getElementById("math").value;
+     bio = document.getElementById("bio").value;
+
+
+let sum = parseFloat(phy) + parseFloat(chem) + parseFloat(math) + parseFloat(bio);
+
+// document.getElementById("demo").innerHTML = 'Total marks is = ${sum}';
+document.getElementById("demo").innerHTML = `Total marks is = ${sum}`;
+
+let percentage = (sum/400)*100;
+// document.getElementById("demo1").innerHTML = 'Percentage is =${percentage}%';
+document.getElementById("demo1").innerHTML = `Percentage is = ${percentage}%`;
+
+if(percentage >= 100){
+    document.getElementById("demo2").innerHTML = 'Excellent';
+}else if(percentage >= 80){
+    document.getElementById("demo2").innerHTML = 'Very Good Work';
+}else if(percentage >= 60){
+    document.getElementById("demo2").innerHTML = 'Pass';
+}else if(percentage >= 40){
+    document.getElementById("demo2").innerHTML = 'Need Hard Work';
+}else{
+    document.getElementById("demo2").innerHTML = 'Fail';
+}
+}
